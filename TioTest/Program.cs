05a8049e1d8b2b187707c18841640d1377d230ed
Leonardo Mendes <mendesleo98@gmail.com>
2021-05-11 20:44:27 -0300

    Subindo branch

---
+++ TioTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TioClient;
+
+namespace TioTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection con = new Connection("localhost",2605);
+        }
+    }
+}
